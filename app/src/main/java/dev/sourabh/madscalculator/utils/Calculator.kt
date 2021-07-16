1ff1435c0d33fa5f1af54b54419fcf47c816236e
Sourabh Shegane <sgsourabh30@gmail.com>
2021-07-16 21:04:27 +0530

    Initial commit

---
+++ app/src/main/java/dev/sourabh/madscalculator/utils/Calculator.kt
@@ -0,0 +1,8 @@
+package dev.sourabh.madscalculator.utils
+
+class Calculator {
+
+    fun calculate(expression: String): Int{
+        return 0
+    }
+}
---
+++ app/src/test/java/dev/sourabh/madscalculator/android/CalculatorTest.kt
@@ -0,0 +1,16 @@
+package dev.sourabh.madscalculator.android
+
+import dev.sourabh.madscalculator.utils.Calculator
+import org.junit.Assert
+import org.junit.Test
+
+class CalculatorTest {
+
+    val calculator = Calculator()
+
+    @Test
+    fun checkAgainstEmptyExpression() {
+        val result = calculator.calculate("")
+        Assert.assertEquals(0, result)
+    }
+}
