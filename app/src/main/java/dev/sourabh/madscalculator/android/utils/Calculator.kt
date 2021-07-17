3a146bcc7615316465d5378257c0563241b919a9
Sourabh Shegane <sgsourabh30@gmail.com>
2021-07-17 11:27:34 +0530

    Add Calculator.kt to perform calculations and add CalculatorTest.kt to perform unit tests

--- app/src/main/java/dev/sourabh/madscalculator/android/utils/Calculator.kt
+++ app/src/main/java/dev/sourabh/madscalculator/android/utils/Calculator.kt
@@ -1,8 +1,107 @@
 package dev.sourabh.madscalculator.android.utils
 
+import java.lang.Exception
+import java.util.*
+
 class Calculator {
 
-    fun calculate(expression: String): Int{
-        return 0
+    var operatorStack = ArrayDeque<Char>()
+    var operandStack = ArrayDeque<Int>()
+
+    private fun isCurrentTopHavingGreaterPriority(characterToCheckWith: Char): Boolean {
+        val currentTopPriority = getPriority(operatorStack.peek())
+        val currentCharacterPriority = getPriority(characterToCheckWith)
+        return currentTopPriority >= currentCharacterPriority
+    }
+
+    private fun getPriority(character: Char): Int {
+        return when (character) {
+            '*' -> 4
+            '+' -> 3
+            '/' -> 2
+            else -> 1
+        }
+    }
+
+    private fun performOperation(topOperand: Int, bottomOperand: Int, operator: Char): Int {
+        return when (operator) {
+            '+' -> (bottomOperand + topOperand)
+            '*' -> (bottomOperand * topOperand)
+            '-' -> (bottomOperand - topOperand)
+            else -> (bottomOperand / topOperand)
+        }
+    }
+
+    private fun isCurrentCharacterAnOperator(characterToCheck: Char): Boolean {
+        val operators = listOf('+', '-', '*', '/')
+        return operators.contains(characterToCheck)
+    }
+
+    fun calculate(exp: String): Int {
+        try {
+            if(isExpressionValid(exp)){
+                val numericalString = StringBuilder()
+                for (i in 0 until exp.length) {
+                    val currentCharacter = exp[i]
+                    if (isCurrentCharacterAnOperator(currentCharacter)) {
+                        val numericalStringAsInt = Integer.parseInt(numericalString.toString())
+                        operandStack.push(numericalStringAsInt)
+                        numericalString.delete(0, numericalString.length)
+
+                        if (operatorStack.isEmpty()) {
+                            operatorStack.push(currentCharacter)
+                        } else {
+                            if (isCurrentTopHavingGreaterPriority(currentCharacter)) {
+                                val result = getResult()
+                                operandStack.push(result)
+                                operatorStack.push(currentCharacter)
+
+                            } else {
+                                operatorStack.push(currentCharacter)
+                            }
+                        }
+                    } else {
+                        numericalString.append(currentCharacter)
+                    }
+                }
+
+                val numericalStringAsInt = Integer.parseInt(numericalString.toString())
+                operandStack.push(numericalStringAsInt)
+
+                var i = 0
+                val size = operatorStack.size
+                while (i < size) {
+                    i += 1
+                    val result = getResult()
+                    operandStack.push(result)
+                }
+
+                return operandStack.pop()
+            }else{
+                return -1
+            }
+        }catch (exception: Exception){
+            return -1
+        }
+    }
+
+    private fun getResult(): Int {
+        val topOperand = operandStack.pop()
+        val bottomOperand = operandStack.pop()
+        val operator = operatorStack.pop()
+        val result = performOperation(topOperand, bottomOperand, operator)
+        return result
+    }
+
+    private fun isExpressionValid(expressionToCheck: String): Boolean{
+        if(expressionToCheck.endsWith("+") || expressionToCheck.endsWith("-") || expressionToCheck.endsWith("*") || expressionToCheck.endsWith("/")){
+            return false
+        }
+
+        if (expressionToCheck.startsWith("+") || expressionToCheck.startsWith("-") || expressionToCheck.startsWith("*") || expressionToCheck.startsWith("/")){
+            return false
+        }
+
+        return true
     }
 }
--- app/src/test/java/dev/sourabh/madscalculator/android/CalculatorTest.kt
+++ app/src/test/java/dev/sourabh/madscalculator/android/CalculatorTest.kt
@@ -9,8 +9,56 @@ class CalculatorTest {
     val calculator = Calculator()
 
     @Test
-    fun checkAgainstEmptyExpression() {
-        val result = calculator.calculate("")
-        Assert.assertEquals(0, result)
+    fun checkSimpleExpression() {
+        val actualResult = calculator.calculate("5+2")
+        Assert.assertEquals(7, actualResult)
+    }
+
+    @Test
+    fun checkAgainstExpressionWithOperatorAtLast() {
+        val actualResult = calculator.calculate("5+2*")
+        Assert.assertEquals(-1, actualResult)
+    }
+
+    @Test
+    fun checkAgainstExpressionWithOperatorAtStart() {
+        val actualResult = calculator.calculate("/5+2")
+        Assert.assertEquals(-1, actualResult)
+    }
+
+    @Test
+    fun checkAgainstExpressionWithOperatorsAtBothEnd() {
+        val actualResult = calculator.calculate("/5+2-")
+        Assert.assertEquals(-1, actualResult)
+    }
+
+    @Test
+    fun checkAgainstInvalidExpression() {
+        val actualResult = calculator.calculate("/5++2-")
+        Assert.assertEquals(-1, actualResult)
+    }
+
+    @Test
+    fun checkExpressionWithMADSequence() {
+        val actualResult = calculator.calculate("50+20/10")
+        Assert.assertEquals(7, actualResult)
+    }
+
+    @Test
+    fun checkExpressionWithOnlyAdditionOperator() {
+        val actualResult = calculator.calculate("50+20+10")
+        Assert.assertEquals(80, actualResult)
+    }
+
+    @Test
+    fun checkExpressionWithOnlySubtractOperator() {
+        val actualResult = calculator.calculate("10-2-3")
+        Assert.assertEquals(5, actualResult)
+    }
+
+    @Test
+    fun checkExpressionWithOnlyMultiplicationOperator() {
+        val actualResult = calculator.calculate("10*2*8")
+        Assert.assertEquals(160, actualResult)
     }
 }
