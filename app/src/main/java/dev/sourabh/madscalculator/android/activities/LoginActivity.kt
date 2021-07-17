252f5281cd36ec9ca9f4e90b6611f3cf2df91cbe
Sourabh Shegane <sgsourabh30@gmail.com>
2021-07-17 20:46:29 +0530

    Added correct email and password details in LoginActivity

--- app/src/main/java/dev/sourabh/madscalculator/android/activities/LoginActivity.kt
+++ app/src/main/java/dev/sourabh/madscalculator/android/activities/LoginActivity.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
